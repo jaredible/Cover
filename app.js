c7d76b867cbc1948dc2fed72fa7a5908139f6af1
Jared Diehl <jareddiehl96@gmail.com>
2019-01-01 14:46:48 -0600

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const process = require('process');
 const app = express();
 const server = require('http').Server(app);
 
-const PORT = process.env.port || 8000;
+const PORT = process.env.port || 1000;
 const HOST = process.env.host || 'localhost';
 const ENV = app.get('env');
 
