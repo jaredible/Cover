965a51468c42fccad7c0666799c28915ff30ac04
Jared Diehl <jareddiehl96@gmail.com>
2019-01-01 21:12:21 -0600

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,5 @@
 $(function () {
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+        $('#card').append('Testing');
+    }
 });
